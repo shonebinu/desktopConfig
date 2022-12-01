17e8925f0d31357de88c6aa2e7bcd9236aaab0cb
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2022-12-01 21:20:05 +0530

    Delete .config directory

--- .config/qtile/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-xrandr --output HDMI-A-0 --primary --mode 1920x1080 --pos 1080x340 --rotate normal --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate left --output DisplayPort-0 --off &
-
-picom -b &
-
-nitrogen --restore &
-
-pragha -p -f -u -x
