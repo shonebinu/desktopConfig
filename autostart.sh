5077827be15851f31fe431b58ce552ba64312c09
Shone Binu <shonebinualias@gmail.com>
2022-12-18 22:49:55 +0530

    cp

--- autostart.sh
+++ autostart.sh
@@ -1,2 +1,10 @@
+#!/usr/bin/env bash
+
 mpv --no-video --volume=25 --shuffle /home/shonebinu/Music/Lofi/* & #Control volume with playerctl
-cp -r /home/shonebinu/Documents/Shone\'s\ Vault/ /media/shonebinu/Backup\ Plus/
+
+while true
+do
+ cp -r /home/shonebinu/Documents/Shone\'s\ Vault/ /media/shonebinu/Backup\ Plus/ 
+ sleep 1800
+done
+
