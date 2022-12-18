11eed2ed395cad892e7c520107146766f607200b
Shone Binu <shonebinualias@gmail.com>
2022-12-18 22:23:53 +0530

    obsidian sync

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,2 @@
-#!/usr/bin/env bash
-
-# Install MPV for music
-mpv --no-video --volume=25 --shuffle /home/shonebinu/Music/Lofi/*
+mpv --no-video --volume=25 --shuffle /home/shonebinu/Music/Lofi/* & #Control volume with playerctl
+cp -r /home/shonebinu/Documents/Shone\'s\ Vault/ /media/shonebinu/Backup\ Plus/
